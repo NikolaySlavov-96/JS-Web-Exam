ab8fae9f06cd3addde980bb2d6124baf464821a1
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-11-26 13:24:56 +0200

    update of taken and using data in dbconfig module

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,21 +1,20 @@
 require('dotenv').config();
 const mongoDB = require('mongoose');
 
-// const CONNECT_STRING = 'mongodb://localhost:27017/';
-const CONNECT_STRING = process.env.DB_ADDRESS;
-
-const DB_NAME = process.env.DB_NAME;
 
 module.exports = async (app) => {
     try {
-        await mongoDB.connect(CONNECT_STRING + DB_NAME, {
+        await mongoDB.connect(process.env.DB_ADDRESS, {
+            useUnifiedTopology: true,
             useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Data Base connecter with ' + DB_NAME);
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASSWORD,
+            dbName: process.env.DB_NAME,
+        })
+        console.log('Data Base connecter with ');
 
     } catch (err) {
         console.error(err);
-        process.exit(1);
+        // process.exit(1);
     }
 }
